52bcc7828c9c94658513e213cf87a24abb8c13d9
BSKOM <b.shelestua@gmail.com>
2021-02-28 15:48:39 +0200

    init: add check function v 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+    let arr = bracketsConfig.map(e => e.join(''))
+    for (let i = str.length; str.length;) {
+      for (let value of arr) str = str.replace(value, '')
+      if ((i === str.length) && str.length) return false
+      i = str.length
+    }
+    return true
+  }
 }
